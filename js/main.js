7bb77531ce88bcc1b4a3d6db8fdf69f68413ddbf
Christopher Lacaille <Christopher Lacaille>
2016-05-26 04:52:51 -0500

    and functionaliy and style to jumbotron

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 $(function(){ 
 	
-	let $astronaut = $("#astronaut"),
+	var $astronaut = $("#astronaut"), // astronaught is 150px wide
+		$astronautHeight = $astronaut.height(),
+		$astronautWidth = $astronaut.width(),
 		$jumbotron = $(".jumbotron"),
-		$window = $(window),
-		$endHere = $(".jumbotron").offset().top - $window.height();
+		$jumbotronHeight = $jumbotron.height(),
+		$jumbotronWidth = $jumbotron.width(),
+		$window = $(window);
 
-		//console.log($(".jumbotron").offset().top);
+console.log($astronautWidth);
 
-	$window.on("scroll", function() {
-		let newLeft = Math.random() * $window.innerWidth(),
-			newTop = Math.random() * $jumbotron.height();
-		$astronaut.stop(true).animate({
-			left: newLeft,
-			top: newTop
-		}, 2200);
+	$jumbotron.on("mouseover click touch",  function(e) {
+		var newLeft = Math.random() * ($jumbotronWidth - $astronautWidth),
+			newTop = Math.random() * ($jumbotronHeight - $astronautHeight);
+
+		$("#astronaut").stop(true).animate({
+			left: e.pageX - 80 ,
+			top: e.pageY - 250
+		}, 2000);
+
 	});
-
-
-
 
 
 });
