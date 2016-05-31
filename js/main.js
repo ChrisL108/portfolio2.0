a9ab33c3283c87df9c6494c6986e1fbaccd70615
Christopher Lacaille <Christopher Lacaille>
2016-05-31 02:54:25 -0500

    add #what-i-do and update styling on skills

--- js/main.js
+++ js/main.js
@@ -13,11 +13,11 @@ $(".jumbotron").mousemove(function(e){
 });
 
 // Loop over astronaut Tween animation
-// setInterval & Tween ( or css() ) is smoother than jQuery's animation()
+// setInterval & Tween ( or css() ) is smoother than animation()
 var loop = setInterval(function(){
 	// Damper on 15 .. can change ... higher is slower
-    	xp += (mouseX - xp) / 15;
-		yp += (mouseY - yp) / 15;
+    	xp += (mouseX - xp) / 13;
+		yp += (mouseY - yp) / 13;
 
 	    if ( xp > 50 && xp < ($window.width() - 150) && yp > 0 && yp < 460) {
 	    	TweenLite.to(astronaut, 1.5, {
@@ -35,13 +35,28 @@ var loop = setInterval(function(){
 $skills = $("#skills-section");
 $window.on('scroll', function() {
 	if ( $window.scrollTop() > $skills.offset().top -
-										 $skills.height() / 2 ) {
-		$(".progress:eq(0)").animate({ value: '90' }, 1000);
-		$(".progress:eq(1)").animate({ value: '85' }, 2000);
-		$(".progress:eq(2)").animate({ value: '68' }, 3000);
-		$(".progress:eq(3)").animate({ value: '73' }, 4000);
+									( $skills.height() / 2 )  ){
+		$(".progress:eq(0)").animate({ value: '92' }, 1000); //JS
+		$(".progress:eq(1)").animate({ value: '83' }, 2000); //HTML
+		$(".progress:eq(2)").animate({ value: '68' }, 3000); //CSS
+		$(".progress:eq(3)").animate({ value: '75' }, 4000); //etc
+		progressText.fadeIn();
 	}
 });
+
+var progressText = $(".progress-text");
+var showText = function() {
+	progressText.fadeIn('slow');
+}
+progressText.hide();
+
+// animate by letter?
+// var titles = $("#what-i-do h4");
+// for (var i=0; i< titles.length; i++ ) {
+// 	for (var j = 0; j < titles[i].length ; j++ ) {
+// 		console.log(titles[i][j].val());
+// 	}
+// }
 
 
 });   // end ready function
