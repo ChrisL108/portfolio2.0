8df79775d5f5485f846e45fcd56911c57fe9cf91
Christopher Lacaille <Christopher Lacaille>
2016-06-22 01:02:29 -0500

    add smooth scrolling function to main.js

--- js/main.js
+++ js/main.js
@@ -46,6 +46,25 @@ $window.on('scroll', function() {
 });
 
 
+// smoother scrolling
+$(document).ready(function(){
+  $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
+      }
+    }
+  });
+});
+
+
 });   // end ready())
 
 
