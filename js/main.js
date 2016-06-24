5b32cf5c2c8d1882b5136eaa569a217e28592fba
Christopher Lacaille <Christopher Lacaille>
2016-06-24 05:16:56 -0500

    add resume

--- js/main.js
+++ js/main.js
@@ -34,38 +34,43 @@ var $about = $("#about"), $about_text = $('#about_text'),
 		$about_text2 = $('#about_text2'), $about_text3 = $('#about_text3');
 
 $window.on('scroll', function() {
-	if ( $window.scrollTop() > $about.offset().top - ( $about.height() / 2 )  ){
-		$about_text.slideDown('fast').delay(800);
-		$about_text2.fadeIn('slow').delay(1000);;
-    $about_text3.fadeIn('slow');
-      
-		
-	} else {
-		$about_text.slideUp('fast', function() {
-			$about_text2.slideUp('fast', function() {
-        $about_text3.slideUp('fast');
-      });
+
+	if ( $window.scrollTop() > $about.offset().top - ( $about.height() / 1.2 )) {
+		$about_text.fadeIn(300, function() { 
+			$about_text2.fadeIn(1100, function() {
+				$about_text3.fadeIn(300);
+			});
 		});
+	} else {
+		$about_text.hide();
+		$about_text2.hide();
+		$about_text3.hide();
 	}
+
 });
 
+// $("#to-top").on('click', function(event) {
+// 	event.preventDefault();
+// 	$(‘html, body’).animate({scrollTop: 0});
+// 	return false;
+// });
 
 // smoother scrolling
 $(document).ready(function(){
-  $('a[href*=#]').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-    && location.hostname == this.hostname) {
-      var $target = $(this.hash);
-      $target = $target.length && $target
-      || $('[name=' + this.hash.slice(1) +']');
-      if ($target.length) {
-        var targetOffset = $target.offset().top;
-        $('html,body')
-        .animate({scrollTop: targetOffset}, 1000);
-       return false;
-      }
-    }
-  });
+	$('a[href*=#]').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+			&& location.hostname == this.hostname) {
+				var $target = $(this.hash);
+				$target = $target.length && $target
+						  || $('[name=' + this.hash.slice(1) +']');
+				if ($target.length) {
+					var targetOffset = $target.offset().top;
+					$('html,body')
+						.animate({scrollTop: targetOffset}, 1000);
+					return false;
+				}
+		}
+	});
 });
 
 
